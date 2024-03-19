b9ed820e84eb43615b6654f10566bb251a7f3619
Carlos Olivo <carlosengel011@gmail.com>
2024-03-19 01:23:57 -0400

    test fetch

--- catalog-service/src/main.ts
+++ catalog-service/src/main.ts
@@ -3,6 +3,7 @@ import { ProductRepository } from "../src/services/product-repository.ts";
 import { seed } from "../src/utils/seed.ts";
 import { Hono } from "https://deno.land/x/hono@v4.0.0-rc.4/mod.ts";
 
+await fetch("https://google.com");
 const productLoader = new FileProductLoader("products.json");
 
 const productRepository = new ProductRepository(productLoader);
